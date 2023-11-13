59eb667651f0632286da6a6909431f2cdf935470
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-13 13:55:04 +0100

    Add project files.

---
+++ moodleOppgave03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace moodleOppgave03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            decimal b = 3.0m;
+
+            // Variabelen må enten være:
+            decimal sum = a + b;
+            // Eller:
+            var sumAlternativ = a + b;
+
+            Console.WriteLine(sum);
+            Console.WriteLine(sumAlternativ);
+
+            Console.ReadKey();
+        }
+    }
+}
